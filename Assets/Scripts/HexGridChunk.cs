b3a771165ac4ca46309764deed2648b5996942de
Ramonnn <ramon.eliens@gmail.com>
2017-09-08 22:27:40 +0200

    Chunks 0.1

---
+++ Assets/Scripts/HexGridChunk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGridChunk : MonoBehaviour
+{
+    HexCell[] cells;
+
+    HexMesh hexMesh;
+    Canvas gridCanvas;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        cells = new HexCell[HexMetrics.chunkSizeX * HexMetrics.chunkSizeZ];
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    public void AddCell(int index, HexCell cell)
+    {
+        cells[index] = cell;
+        cell.transform.SetParent(transform, false);
+        cell.uiRect.SetParent(gridCanvas.transform, false);
+    }
+}
