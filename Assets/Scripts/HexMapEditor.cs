0ebf09d194c909974adf2e941fc1093b0a79c55e
Ramonnn <ramon.eliens@gmail.com>
2017-09-12 22:02:39 +0200

    Small Saving & Loading Changes

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -72,10 +72,9 @@ public class HexMapEditor : MonoBehaviour {
         string path = Path.Combine(Application.persistentDataPath, "test.map");
         using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
         {
+            writer.Write(0);
             hexGrid.Save(writer);
-        }
-
-        
+        }     
     }
 
     public void Load()
@@ -83,7 +82,15 @@ public class HexMapEditor : MonoBehaviour {
         string path = Path.Combine(Application.persistentDataPath, "test.map");
         using (BinaryReader reader = new BinaryReader(File.OpenRead(path)))
         {
-            hexGrid.Load(reader);
+            int header = reader.ReadInt32();
+            if (header == 0)
+            {
+                hexGrid.Load(reader);
+            }
+            else
+            {
+                Debug.LogWarning("Unknown map format " + header);
+            }
         }
     }
 }
