8156aa336f8b289aad67ff132bbaae71ba6adb7f
Ramonnn <ramon.eliens@gmail.com>
2017-09-13 12:13:03 +0200

    Final Search Efficiency improvements i.e. cells outside of the frontier are no longer considered.

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -7,6 +7,7 @@ public class HexCell : MonoBehaviour {
     public HexCell PathFrom { get; set; }
     public int SearchHeuristic { get; set; }
     public HexCell NextWithSamePriority { get; set; }
+    public int SearchPhase { get; set; }
 
     public HexCoordinates coordinates;
     public RectTransform uiRect;
--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -20,6 +20,10 @@ public class HexGrid : MonoBehaviour {
     HexGridChunk[] chunks;
 
     HexCellPriorityQueue searchFrontier;
+    int searchFrontierPhase;
+
+    HexCell currentPathFrom, currentPathTo;
+    bool currentPathExists;
 
     void Awake ()
     {
@@ -103,8 +107,7 @@ public class HexGrid : MonoBehaviour {
 		}
 
 		Text label = Instantiate<Text>(cellLabelPrefab);
-		label.rectTransform.anchoredPosition =
-			new Vector2(position.x, position.z);
+		label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
 		label.text = cell.coordinates.ToStringOnSeparateLines();
         cell.uiRect = label.rectTransform;
 
@@ -132,11 +135,50 @@ public class HexGrid : MonoBehaviour {
 
     public void FindPath(HexCell fromCell, HexCell toCell, int speed)
     {
-        Search(fromCell, toCell, speed);
+        ClearPath();
+        currentPathFrom = fromCell;
+        currentPathTo = toCell;
+        currentPathExists = Search(fromCell, toCell, speed);
+        ShowPath(speed);
     }
 
-    void Search(HexCell fromCell, HexCell toCell, int speed)
+    void ShowPath(int speed)
     {
+        if (currentPathExists)
+        {
+            HexCell current = currentPathTo;
+            while (current != currentPathFrom)
+            {
+                int turn = current.Distance / speed;
+                current.SetLabel(turn.ToString());
+                current.EnableHighlight(Color.white);
+                current = current.PathFrom;
+            }
+        }
+        currentPathFrom.EnableHighlight(Color.blue);
+        currentPathTo.EnableHighlight(Color.red);
+    }
+
+    void ClearPath()
+    {
+        if (currentPathExists)
+        {
+            HexCell current = currentPathTo;
+            while (current != currentPathFrom)
+            {
+                current.SetLabel(null);
+                current.DisableHighlight();
+                current = current.PathFrom;
+            }
+            current.DisableHighlight();
+            currentPathExists = false;
+        }
+        currentPathFrom = currentPathTo = null;
+    }
+
+    bool Search(HexCell fromCell, HexCell toCell, int speed)
+    {
+        searchFrontierPhase += 2;
         if (searchFrontier == null)
         {
             searchFrontier = new HexCellPriorityQueue();
@@ -145,30 +187,33 @@ public class HexGrid : MonoBehaviour {
         {
             searchFrontier.Clear();
         }
-        for (int i = 0; i < cells.Length; i++)
-        {
-            cells[i].Distance = int.MaxValue;
-            cells[i].SetLabel(null);
-            cells[i].DisableHighlight();
-        }
-        fromCell.EnableHighlight(Color.blue);
+        //for (int i = 0; i < cells.Length; i++)
+        //{
+            //cells[i].Distance = int.MaxValue;
+        //    cells[i].SetLabel(null);
+        //    cells[i].DisableHighlight();
+        //}
+        //fromCell.EnableHighlight(Color.blue);
+        fromCell.SearchPhase = searchFrontierPhase;
         fromCell.Distance = 0;
         searchFrontier.Enqueue(fromCell);
         while (searchFrontier.Count > 0)
         {
             HexCell current = searchFrontier.Dequeue();
+            current.SearchPhase += 1;
 
             if (current == toCell)
             {
-                while (current != fromCell)
-                {
-                    int turn = current.Distance / speed;
-                    current.SetLabel(turn.ToString());
-                    current.EnableHighlight(Color.white);
-                    current = current.PathFrom;
-                }
-                toCell.EnableHighlight(Color.red);
-                break;
+                return true;
+                //while (current != fromCell)
+                //{
+                //    int turn = current.Distance / speed;
+                //    current.SetLabel(turn.ToString());
+                //    current.EnableHighlight(Color.white);
+                //    current = current.PathFrom;
+                //}
+                //toCell.EnableHighlight(Color.red);
+                //break;
             }
 
             int currentTurn = current.Distance / speed;
@@ -176,7 +221,7 @@ public class HexGrid : MonoBehaviour {
             for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
             {
                 HexCell neighbor = current.GetNeighbor(d);
-                if (neighbor == null) // ANY TERRAIN MOVEMENT PENALTIES CAN BE ADDED HERE
+                if (neighbor == null || neighbor.SearchPhase > searchFrontierPhase) // ANY TERRAIN MOVEMENT PENALTIES CAN BE ADDED HERE
                 {
                     continue;
                 }
@@ -196,8 +241,9 @@ public class HexGrid : MonoBehaviour {
                 {
                     distance = turn * speed + moveCost;
                 }
-                if (neighbor.Distance == int.MaxValue)
+                if (neighbor.SearchPhase < searchFrontierPhase)
                 {
+                    neighbor.SearchPhase = searchFrontierPhase;
                     neighbor.Distance = distance;
                     neighbor.PathFrom = current;
                     neighbor.SearchHeuristic = neighbor.coordinates.DistanceTo(toCell.coordinates);
@@ -212,6 +258,7 @@ public class HexGrid : MonoBehaviour {
                 }
             }
         }
+        return false;
     }
 
     public void Save(BinaryWriter writer)
@@ -224,6 +271,7 @@ public class HexGrid : MonoBehaviour {
 
     public void Load(BinaryReader reader)
     {
+        ClearPath();
         for (int i = 0; i < cells.Length; i++)
         {
             cells[i].Load(reader);
